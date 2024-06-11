cd1a2bf089e5224c8435eb50f5e80d8dbdada9c0
spatialsymmetry <spatialsymmetry@gmail.com>
2024-06-12 02:44:48 +0330

    compelete redesing 02

--- pages/index.vue
+++ pages/index.vue
@@ -33,9 +33,9 @@
       <p class="text-gray-500 px-3 py-1 text-xs md:text-sm">
       We select projects not solely for their financial return, but for their alignment with our intellectual interests. We primarily focus on solving simple problems—or a collection of simple problems—using a strictly first-principles and modular approach, aiming for solutions that will remain effective for decades to come. Currently, our focus is more on software.</p>
 
-      <textarea rows="1" placeholder="Please introduce yourself." class="w-full bg-gray-300 rounded p-2 text-sm"></textarea>
-      <textarea rows="2" placeholder="Tell us about the problem you're trying to solve." class="w-full bg-gray-300 rounded p-2 text-sm"></textarea>
-      <textarea rows="1" placeholder="How could we contact you?" class="w-full bg-gray-300 rounded p-2 text-sm"></textarea>
+      <textarea rows="1" placeholder="Please introduce yourself." class="bg-gray-300 rounded mx-2 p-2 text-sm"></textarea>
+      <textarea rows="1" placeholder="Tell us about the problem you're trying to solve." class="bg-gray-300 rounded mx-2 p-2 text-sm"></textarea>
+      <textarea rows="1" placeholder="How could we contact you?" class="bg-gray-300 rounded mx-2 p-2 text-sm"></textarea>
       <p class="text-gray-500 px-3 py-1 text-xs md:text-sm"> # Please explain in length. You can expand input fields by dragging down from bottom-right corner.
       </p>
 
